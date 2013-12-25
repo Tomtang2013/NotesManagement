061f46f293918fa7191fe89c6ddf4023dbdd8769
Tom tang <tom.tang@perficientgdc.com.cn>
2013-12-25 16:53:12 +0800

    new version

--- sites/all/themes/billsmanagement/templates/page--billssummary.tpl.php
+++ sites/all/themes/billsmanagement/templates/page--billssummary.tpl.php
@@ -9,6 +9,9 @@
            print drupal_render($navigation_tree); ?>
         </div>
         <div id="pr_dashboard_content_right" class="pr_content_right">
+             <?php if ($messages): ?>
+                  <?php print $messages; ?>
+              <?php endif; ?>
             <?php  print drupal_render($page['content']); ?>
         </div>
     </div>
