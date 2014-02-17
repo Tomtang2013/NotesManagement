68ca014f922a492d2533d5dfe33dfe179ba38af2
Tom tang <tom.tang@perficientgdc.com.cn>
2014-02-17 10:30:41 +0800

    add user limit

--- sites/all/themes/billsmanagement/templates/header.tpl.php
+++ sites/all/themes/billsmanagement/templates/header.tpl.php
@@ -9,9 +9,6 @@
             <h2><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a></h2>
             <p><?php if ($site_slogan): ?><?php print $site_slogan; ?><?php endif; ?></p>
         </div>
-        <div id="login_user" style="float: right; padding: 30px 0 0 10px;">
-            Hi, <?php global $user; echo $user->name; ?>
-        </div>
     </hgroup>
 </header>
 
